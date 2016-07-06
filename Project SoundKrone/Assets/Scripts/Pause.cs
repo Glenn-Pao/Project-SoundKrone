c11563ce434d9f8d638f52e6fca7975c028b1eb5
Glenn-Pao <140522J@mymail.nyp.edu.sg>
2016-07-06 15:15:42 +0800

    Added "PAUSED" GUI. Now to make the pause menu more interactive.

---
+++ Project SoundKrone/Assets/Scripts/Pause.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//a class that handles the pause o3o
+//I think this is overkill though.
+public class Pause : MonoBehaviour 
+{
+    public Renderer sprite;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        ShowSprite();
+	}
+
+    public void ShowSprite()
+    {
+        if(Controller.isPaused)
+        {
+            sprite.material.color = Color.white;
+        }
+        else
+        {
+            sprite.material.color = Color.clear;
+        }
+    }
+}
